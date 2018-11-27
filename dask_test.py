04816d510a9e3c57d3162be59c207b3337ce5137
TomMonks <thomas.m.monks@gmail.com>
2018-11-27 22:22:37 +0000

    Multi-dataset bootstrap.  Renamed single-dataset-boot. Bug fix

--- dask_test.py
+++ dask_test.py
@@ -37,5 +37,30 @@ def draw_sample(data):
     u = np.random.uniform(0, data.shape[0]-1)
     u = round(u)
     return data[u]
+
+
+#remebering how to using indexing in multi-d arrays!
+x = [1, 2, 3]
+y = [4, 5, 6]
+
+z =np.zeros((3, 4))
+#np.append(z, x, 4xis=0)
+z[0][3] = 4
+z[1][3] = 4
+z[2][3] = 4
+
+
+z[0][2] = 3
+z[1][2] = 3
+z[2][2] = 3
+
+print(z)
+
+design = 2
+
+z.T[design:design+1] = x
+z
+
+#np.append(z, x, axis=1)
     
     
